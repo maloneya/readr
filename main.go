2e34695ad9097723c678b49c07b3849ea0822d63
Alex Maloney <maloney.a12@gmail.com>
2018-05-06 18:09:50 -0400

    look up port env var

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+var port = os.Getenv("PORT")
 
 func rootHanderler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello Heroku!")
 }
 
 func main() {
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/", rootHanderler)
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	log.Fatal(http.ListenAndServe(":" + port, nil))
 }
